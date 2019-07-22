8a353985ec00366df11df0cadc4d370ee63a9778
rajasekharchallari009 <challariraj@gmail.com>
2019-07-21 20:33:16 -0400

    EgiantsTasks: Spring Kafka Project Implemented

---
+++ src/main/java/com/egiantsprojects/practice/springkafka/controller/KafkaController.java
@@ -0,0 +1,22 @@
+package com.egiantsprojects.practice.springkafka.controller;
+
+import com.egiantsprojects.practice.springkafka.service.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+	private final Producer producer;
+	@Autowired
+	public KafkaController(Producer producer) {
+		this.producer = producer;
+	}
+	@PostMapping(value = "/publish")
+	public void sendMessageToKafkaTopic(@RequestParam("message") String message){
+		this.producer.sendMessage(message);
+	}
+}
